c9cc4d260880c9edf7d0b08d29969f16f4c7c9c9
natt_nawarat <natt.nawarat@gmail.com>
2023-03-22 17:17:11 +0700

    add cehck answer

--- beginner-v3/word-masters/word-masters.js
+++ beginner-v3/word-masters/word-masters.js
@@ -18,14 +18,12 @@ let currGuessLength = 0
 let currGuessAttempt = 0
 let currGuess = ""
 const addLetter = (letter) => {
-    console.log(currGuessLength)
     if (currGuessLength >= 5) {
         // reach word length limit, do nothing
         return
     }
     const letterId = `#letter-${currGuessLength + currGuessAttempt * 5}`
     currGuess = currGuess + letter
-    console.log(currGuess)
     document.querySelector(letterId).innerText = letter
     currGuessLength += 1
 }
@@ -39,17 +37,30 @@ const deleteLetter = () => {
     document.querySelector(letterId).innerText = ""
     currGuessLength -= 1
 }
-const checkAnswer = (currGuess) =>{
+const checkAnswer = (guess, guessAttempt) => {
     // check ans change style
+    for (let i = 0; i < 5; i++) {
+        let addClass = "wrong" // correct,close,wrong
+        if (word.indexOf(guess[i]) > -1) {
+            addClass = "close"
+        }
+        if (guess[i] === word[i]) {
+            addClass = "correct"
+        }
+        // change letter style
+        const letterId = `#letter-${i + guessAttempt * 5}`
+        const element = document.querySelector(letterId)
+        element.classList.add(addClass)
+    }
 }
 const submitGuess = () => {
-    if (currGuessLength !==5){
+    if (currGuessLength !== 5) {
         // word not complete do nothing
         return
     }
     console.log("submit answer")
     // check answer
-
+    checkAnswer(currGuess,currGuessAttempt)
     // set new state
     currGuessAttempt += 1
     currGuessLength = 0
